63e899ceb2974fac92236bb57f13afae549974dc
Tiago Pierre <tiago_pierre@outlook.com>
2021-02-24 22:55:42 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/components/Profile.module.css";
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/tiagopierre.png" alt="Tiago Pierre" />
+      <div>
+        <strong>Tiago Pierre</strong>
+        <p>
+          <img src="icons/level.svg" alt="" /> Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
